ba62da0d74c9ec73d654153114dd7522be219fcf
julian <julian.merlo@naranja.com>
2020-04-16 21:21:12 -0300

    fix: update conponents collections

--- src/components/Home/Collections/Collections.js
+++ src/components/Home/Collections/Collections.js
@@ -5,16 +5,18 @@ import './Collections.css'
 const Collections = () => {
   return (
     <section className="container-primary">
-      <h3 className="style-font">Colecciones: De fiesta</h3>
+      <div>
+        <h3 className="style-font">Colecciones: De fiesta</h3>
+      </div>
       <div className="container-collection">
-        <div className="container-text shadow">
+        <div className="container-left shadow">
           <img src={deFiesta}></img>
           <div>
             <h5>Descubri</h5>
             <h4>De fiesta</h4>
           </div>
         </div>
-        <div className="list-imagen">
+        <div className="container-right">
           {arrayImg.map((img, index) => <img key={index} src={img}></img>)}
         </div>
       </div>
