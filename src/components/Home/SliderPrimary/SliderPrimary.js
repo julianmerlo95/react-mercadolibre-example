450cb037a7898ca37df051c0f046c9ac112bef9d
julian <julian.merlo@naranja.com>
2020-04-23 14:16:16 -0300

    fix: update component slider

--- src/components/Home/SliderPrimary/SliderPrimary.js
+++ src/components/Home/SliderPrimary/SliderPrimary.js
@@ -4,16 +4,23 @@ import './SliderPrimary.sass';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { settings, arrayImg } from './data';
+import { Context } from '../../../context/Context';
+
 
 class SliderPrimary extends Component {
 
+  static contextType = Context;
   render() {
+    console.log(this.context);
+
+
     return (
-      <section className="carrousel">
+      <section className={`carrousel${this.context.toggleMood === true ? '--background' : ""}`}>
         <Slider {...settings} >
           {arrayImg.map((item, index) =>
             <div key={index}>
-              <img className="carrousel__img" src={item} alt="alt sliderPrimary"></img>
+              <img className={`carrousel__img ${this.context.toggleMood === true ? '--background' : ""}`}
+                src={item} alt="alt sliderPrimary"></img>
             </div>)}
         </Slider>
       </section>
